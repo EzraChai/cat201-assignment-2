73e3e1c76513f7ce9d1409d327ba533d7ecd58e7
Ezra Chai <juanzhe2@gmail.com>
2025-12-08 12:51:10 +0800

    Update NotFound component to adjust margin for Home link

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -13,7 +13,7 @@ function NotFound() {
         <Link
           to={"/"}
           className={
-            " mt-4 bg-white border-2 p-2 md:p-4 border-black md:px-4 md:py-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] hover:translate-x-px hover:translate-y-px hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all md:min-h-8 flex items-center"
+            " mt-12 bg-white border-2 p-2 md:p-4 border-black md:px-4 md:py-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] hover:translate-x-px hover:translate-y-px hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all md:min-h-8 flex items-center"
           }
         >
           <h1 className="font-medium text-xs md:text-base">Home</h1>
