8539c1cf149cef87498d81f6b454327007e630c8
Ezra Chai <juanzhe2@gmail.com>
2025-12-08 10:15:50 +0800

    Update dependencies and enhance UI components with new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { LaptopMinimal } from "lucide-react";
 import "./App.css";
 import NavBar from "./NavBar";
 
@@ -15,8 +16,33 @@ function App() {
       </section>
       <hr className="border-2 border-black" />
       <section className="max-w-7xl mx-auto">
-        <h2 className="text-center mt-4 font-black text-3xl">WHAT DO WE DO</h2>
-        <p className="text-center "></p>
+        <h2 className="text-center mt-6 mb-4 font-black text-3xl">
+          WHAT DO WE DO
+        </h2>
+        <div className="grid md:grid-cols-3 gap-8">
+          <div className="flex flex-col  border-2 border-black bg-white px-4 py-2 ml-2 shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all">
+            {/* <div className=" border-2 border-black bg-white pt-1 px-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:translate-x-px hover:translate-y-px hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-all"> */}
+            <LaptopMinimal className="" />
+            {/* </div> */}
+            <div className="">
+              <h1 className="mt-2 font-black text-xs md:text-2xl">
+                Custom Web Application Development
+              </h1>
+              <p className="font-medium text-base text-neutral-600">
+                We build scalable and efficient web applications tailored to
+                your business needs.
+              </p>
+            </div>
+          </div>
+          <div className=" border-2 border-black bg-white px-4 py-2 ml-2 shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all">
+            <h1 className="font-medium text-xs md:text-base">
+              Mobile App Development (Android & IOS)
+            </h1>
+          </div>
+          <div className=" border-2 border-black bg-white px-4 py-2 ml-2 shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all">
+            <h1 className="font-medium text-xs md:text-base">UI/UX Design</h1>
+          </div>
+        </div>
       </section>
     </>
   );
---
+++ src/Card.tsx
@@ -0,0 +1,7 @@
+function Card() {
+  return (
+    <div className=" border-black px-4 py-2 ml-2 shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all"></div>
+  );
+}
+
+export default Card;
